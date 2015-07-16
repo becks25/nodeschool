7e1e661db6fc5d39233a32d9ba3e16b9d6a33630
Beckylee <Beckylee.Dell@tufts.edu>
2015-07-16 17:23:50 -0400

    completed babysteps, helloworld & myfirstI/0 from learnyounode

---
+++ babySteps.js
@@ -0,0 +1,7 @@
+var sum = 0;
+
+process.argv.slice(2).forEach(function(num){
+	sum += +num;
+});
+
+console.log(sum);
---
+++ myFirstI_O.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+
+var count = fs.readFileSync(process.argv[2]).toString().split('\n').length - 1;
+
+console.log(count);
+
